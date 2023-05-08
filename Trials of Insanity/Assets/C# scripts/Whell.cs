ebc732b3c1ed3368f83a8feb0ba948fdfcdf6198
J-A-Arcos <116697234+J-A-Arcos@users.noreply.github.com>
2023-05-08 08:55:01 -0700

    got the wheel to change when you have picked up rope

--- Trials of Insanity/Assets/C# scripts/Whell.cs
+++ Trials of Insanity/Assets/C# scripts/Whell.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Whell : Collidable
+public class Whell : Collectable
 {
    public Sprite openWhell;
    public Sprite ropeWhell;
    public string sceneName;
+   public rope Rope;
 
    protected override void OnCollide(Collider2D coll)
     {
         if(coll.name == "Player")
         {
            GetComponent<SpriteRenderer>().sprite = openWhell;
-           SceneManager.LoadScene(sceneName);
+           //SceneManager.LoadScene(sceneName);
+           if(Rope.ropeItem == true)
+           {
+                GetComponent<SpriteRenderer>().sprite = ropeWhell;
+           }
+           else
+           {
+                Debug.Log("NO rope!");
+           }
         }
     }
 }
--- Trials of Insanity/Assets/C# scripts/rope.cs
+++ Trials of Insanity/Assets/C# scripts/rope.cs
@@ -2,37 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rope : PlayerInventory
+public class rope : Collectable
 {
     public GameObject objectRope;
-    PlayerInventory playerInventory;
+    public bool ropeItem;
     
     protected override void OnCollect()
     {
         if(!collected)
         {
             collected = true;
+            ropeItem = true;
             objectRope.SetActive(false);
-            if(playerInventory != null)
-            {
-                playerInventory.RopeCollected();
-            }
         }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        playerInventory = other.GetComponent<PlayerInventory>();
-    }
-
-    // private void OnTriggerEnter(Collider other)
-    // {
-    //     PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
-
-    //     if(playerInventory != null)
-    //     {
-    //         playerInventory.RopeCollected();
-    //         objectRope.SetActive(false);
-    //     }
-    // }
 }
